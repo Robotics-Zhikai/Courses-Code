1889eaa6518ffe2eb2cce35ba12c6a51535f6faf
LAPTOP\zhikaiwang <979256120@qq.com>
2020-11-14 13:16:32 +0800

    readme

--- C++ primer/main.cpp
+++ C++ primer/main.cpp
@@ -60,40 +60,85 @@ int inlinetest()
 	return 2;
 }
 
+void NotThrow() throw()
+{
+	throw exception("sss") ;
+}
+
+int & CastConstZK(const int &tmp)
+{
+	return const_cast<int &>(tmp); //把const属性抛掉了，不推荐，因为入口的参数应该是不可改变的
+}
+
 void main()
 {
-	const screen constscreen(5);
-	screen tmpscreen(2);
-	screen tmpscreen1(3);
-	tmpscreen1 = tmpscreen; //似乎编译器自己重载了一个赋值operator
-	constscreen.display() = tmpscreen; //这个非引用 为什么还能做左值 因为编译器把该表达式翻译为了operator = 
-	//在effective C++中有表述
-	constscreen.display().changedata(100); // 不会对constscreen做任何操作
+	try
+	{
+		////这下边这个本来应该是未定义的行为的，但是可能VS2015编译器有一些优化，就可以对声明为const的值进行修改
+		////但我认为，未定义的行为可能是本来声明为const，但是有这些行为使得const的值变化了，造成难以预计的程序运行后果
+		////意味着程序具有设计缺陷，编译可能在所有平台都是可以通过的，只不过与标准相悖
+		//const int j = 3; // j 声明为const常量
+		//int* pj = const_cast<int*>(&j);
+		//*pj = 4;         // 用去const的指针去修改一个原本就是常量的j的值，undefined behavior!
 
-	cout << testinline() << endl;
-	cout << inlinetest() << endl;
-	
-	int a = 5;
-	int * Locate_a = &a;
-	int ** locloc_a = &Locate_a;
-	int *** loclocloc_a = &locloc_a;
-	cout << Locate_a << endl;
-	cout << locloc_a << endl;
-	cout << loclocloc_a << endl;
-	
-	//using namespace Exercise;
-	using Exercise::ivar;
-	//using Exercise::dvar;
-	using Exercise::limit;
-	
-	fuc();
-	using Exercise::cout;
-	std::cout << cout << endl;
+		const int intnum = 5;
+		const int intconst1 = intnum;
+		//intconst1 = 34; //报错必须是可修改的左值，可以通过引用的const_cast来修改 但是很不推荐！
+		//const_cast<int> (intconst1) //报错必须是指针、引用 因为只能修改底层const属性
+		CastConstZK(intconst1) = 34; //这样一下就把本来应该是const的值给修改了
 
-	double dvar = 3.1415;
 
-	int iobj = limit + 1;
-	++ivar;
-	++::ivar;
+		const int * intconst = &intnum;
+		const int & ref = intnum;
+		//const int & &ss = ref; //这样就会报错不能使用对引用的引用
+		const int & ss = ref; //这样不会报错对引用的引用
+		const_cast<int &>(ref) = 100;
+		//int * intnotconstnotcast = intconst;
+		*(const_cast<int *>(intconst)) =1;
+		int * intnotconst = const_cast <int *> (intconst);
+		*intnotconst = 6;
+
+		//NotThrow();
+		vector<int> vectorint(1,1);
+		vectorint.push_back(10);
+		const screen constscreen(5);
+		screen tmpscreen(2);
+		screen tmpscreen1(3);
+		tmpscreen1 = tmpscreen; //似乎编译器自己重载了一个赋值operator
+		constscreen.display() = tmpscreen; //这个非引用 为什么还能做左值 因为编译器把该表达式翻译为了operator = 
+										   //在effective C++中有表述
+		constscreen.display().changedata(100); // 不会对constscreen做任何操作
+
+		cout << testinline() << endl;
+		cout << inlinetest() << endl;
+
+		int a = 5;
+		int * Locate_a = &a;
+		int ** locloc_a = &Locate_a;
+		int *** loclocloc_a = &locloc_a;
+		cout << Locate_a << endl;
+		cout << locloc_a << endl;
+		cout << loclocloc_a << endl;
+
+		//using namespace Exercise;
+		using Exercise::ivar;
+		//using Exercise::dvar;
+		using Exercise::limit;
+
+		fuc();
+		using Exercise::cout;
+		std::cout << cout << endl;
+
+		double dvar = 3.1415;
+
+		int iobj = limit + 1;
+		++ivar;
+		++::ivar;
+	}
+	catch (exception d)
+	{
+		cout << d.what();
+	}
+	
 
 }
