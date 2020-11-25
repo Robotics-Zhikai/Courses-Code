7ba48f7990167cd8a524441ed4f173303a6191df
LAPTOP\zhikaiwang <979256120@qq.com>
2020-11-26 00:52:21 +0800

    readme

--- C++ primer/Code/CP5_ex7_57.h
+++ C++ primer/Code/CP5_ex7_57.h
@@ -10,20 +10,29 @@ class Account
 //银行的宏观调控包括基准利率
 {
 public:
-	Account(std::string Combiinput = DefaultCombination) :ID_card("ID_card:"+std::to_string(++NumID)), Combination(Combiinput) { };
+	Account(std::string HumanIDinput, std::string Combiinput = DefaultCombination) :
+		ID_credit("ID_card:"+std::to_string(++NumID)), Combination(Combiinput),
+		HumanID(HumanIDinput){ };
 	//默认密码是六个0
-	Account(Account& acc) :ID_card("ID_card:" + std::to_string(++NumID)),Combination(DefaultCombination) {}
+	Account(Account& acc) :ID_credit("ID_credit:" + std::to_string(++NumID)),Combination(DefaultCombination) {}
 	//拷贝构造函数，意味着将银行卡的必要信息挪到另一张卡上，相当于重新开了一张卡，这张卡的密码不应该继承上一张卡，因此设置为默认密码
-	const std::string ID_card;
-	const std::string name;
+	const std::string ID_credit;
+	const std::string HumanID; //人的身份证号
+
+	static double read_rate() { return rate; }
+	static void change_rate(double changedrate) { rate = changedrate; }
+	static std::string read_DefaultCombination() { return DefaultCombination; }
 private:
+	double Balence = 0;//存储余额
 	std::string Combination;
 	static long NumID; //表示本张卡是银行发的第NumID张卡
 	//static constexpr std::string DefaultCombination ; //这样会报错constexpr必须具有文本类型和引用类型
 	static const std::string DefaultCombination; //只有static类型才能做默认实参
+	static double rate ;//银行的利率
 };
 
+
 long Account::NumID = 0;
-const std::string Account::DefaultCombination = "000000";
+const std::string Account::DefaultCombination = "000000";//由银行统一决定初始的默认密码是多少
 
 #endif
