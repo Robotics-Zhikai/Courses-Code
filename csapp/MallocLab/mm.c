40076743dc6768bd7b36892c4d1bcea6b0a8898f
Robotics-Zhikai <979256120@qq.com>
2021-03-21 01:06:53 +0800

    csapp Malloc lab subpart1

--- csapp/MallocLab/mm.c
+++ csapp/MallocLab/mm.c
@@ -39,16 +39,33 @@ team_t team = {
 #define ALIGNMENT 8
 
 /* rounds up to the nearest multiple of ALIGNMENT */
-#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7)
+#define ALIGN(size) (((size) + (ALIGNMENT-1)) & ~0x7) //上取整到ALIGNMENT的倍数
 
 
 #define SIZE_T_SIZE (ALIGN(sizeof(size_t)))
+
+#define pack(size,used) (size|used) //将当前结点的总字节大小和是否被使用的标志位结合起来
+                                    //当前结点的总字节大小是ALIGNMENT的倍数
+
+#define headPtr(payload_ptr) ((char*)payload_ptr-SIZE_T_SIZE) 
+#define get(ptr) (*(size_t*)ptr)
+#define put(ptr,value) (*(size_t*)ptr=value)
+#define getSize(payload_ptr) (get(headPtr(payload_ptr))& (~0x7)) //八的倍数 不受是否使用位的影响
+#define footPtr(payload_ptr) ( (char*)payload_ptr + (getSize(payload_ptr)-2*SIZE_T_SIZE) )
+#define setFlag(ptr,pos,flag) (put(ptr,get(ptr)& (~pos)| flag )) //对size_t 数据的pos位设置flag pos选中位，flag在对应的位设置0或1
+// #define setUnused(payload_ptr) (put(headPtr(payload_ptr),pack(getSize(payload_ptr),0))&put(footPtr(payload_ptr),pack(getSize(payload_ptr),0)))
+// #define setused(payload_ptr) (put(headPtr(payload_ptr),pack(getSize(payload_ptr),1)))
+
+#define nextPtr(payload_ptr) ((char*)payload_ptr+getSize(payload_ptr))
+#define prevPtr(payload_ptr) ((char*)payload_ptr - (get((char*)payload_ptr-2*SIZE_T_SIZE)&(~0x7)))
+#define checkUsed(payload_ptr) (get(headPtr(payload_ptr))&0x1)
 
 /* 
  * mm_init - initialize the malloc package.
  */
 int mm_init(void)
 {
+    mem_init();
     return 0;
 }
 
@@ -63,16 +80,56 @@ void *mm_malloc(size_t size)
     if (p == (void *)-1)
 	return NULL;
     else {
-        *(size_t *)p = size;
+        put(p,pack(newsize,0));
         return (void *)((char *)p + SIZE_T_SIZE);
     }
 }
 
+void coalesce(void * ptr) //合并块 这里就不进行合法性检查了 必须在合适的地方调用 也就是当前块必须是空闲块
+{
+    void * headptr = headPtr(ptr);
+    size_t headvalue = get(headptr);
+    int freeblock[2]; //前后的块是空闲块的话置1
+    if (headvalue&0x2) // 说明上一个块是空闲块
+        freeblock[0] = 1;
+    else
+        freeblock[0] = 0;
+    if (checkUsed(nextPtr(ptr)))
+        freeblock[1] = 0;
+    else
+        freeblock[1] = 1;
+    
+    if (freeblock[0]==1&&freeblock[1]==1) //前后都空闲
+    {
+        void * prevPtr = prevPtr(ptr);
+        void * nextPtr = nextPtr(ptr);
+        size_t allsize = getSize(prevPtr)+getSize(ptr)+getSize(nextPtr);
+        void * prevheadPtr = headPtr(prevPtr);
+        void * nextfootPtr = footPtr(nextPtr);
+        put(prevheadPtr,pack(allsize,0));
+        put(nextfootPtr,pack(allsize,0));
+    }
+    else if (freeblock[0]==1)
+    {
+
+    }
+    else if (freeblock[1]==1)
+    {
+
+    }
+    
+}
 /*
  * mm_free - Freeing a block does nothing.
  */
 void mm_free(void *ptr)
 {
+    void * headptr = headPtr(ptr);
+    setFlag(headptr,0x1,0);
+    void * footptr = footPtr(ptr);
+    setFlag(footptr,0x1,0);
+    void * nextheadPtr = headPtr(nextPtr(ptr));
+    setFlag(nextheadPtr,0x2,0x2); //下一个区块的倒数第二位代表上一个区块是否是空，如果为空，则置1 这个机制是为了减少内部碎片，当块分配了时，不需要foot
 }
 
 /*
@@ -80,6 +137,7 @@ void mm_free(void *ptr)
  */
 void *mm_realloc(void *ptr, size_t size)
 {
+    return realloc(ptr,size);
     void *oldptr = ptr;
     void *newptr;
     size_t copySize;
@@ -95,7 +153,10 @@ void *mm_realloc(void *ptr, size_t size)
     return newptr;
 }
 
-
+int mm_check() //检查堆一致性 用来帮助debug的，在提交程序中不应包括这个函数，会降低吞吐量
+{
+    return 0;
+}
 
 
 
